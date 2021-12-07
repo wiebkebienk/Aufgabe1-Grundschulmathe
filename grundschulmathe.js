db6d261d7c79539416f820bde64777c605aa0ca9
Wiebke Bienk <wiebke.bienk@gmail.com>
2021-12-07 09:59:24 +0100

    Aufgabe b erledigt

--- grundschulmathe.js
+++ grundschulmathe.js
@@ -1,2 +1,28 @@
 //----Aufgabe A----//
 alert("Grundschulmathe");
+
+//----Aufgabe B----//
+
+//Zufallszahl (zwischen 1 und 20)
+function randomNum() {
+    return Math.floor(Math.random() * 21);
+}
+
+//Funktion Rechenaufgabe
+function sum() {
+    var num1 = randomNum();
+    var num2 = randomNum();
+    var solution = num1 + num2;
+    var userAnswer = parseInt(prompt("Wie viel ist " + num1 + " + " + num2 + " ?"), 10);
+    
+//Antwort, ob die Aufgabe richtig ist
+    if (userAnswer === solution) {
+        alert("Richtig!");
+    }
+    else {
+        alert("Falsch!");
+    }
+}
+
+//Aufruf Funktion Matheaufgabe
+sum();
