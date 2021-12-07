0970d5045ef256c66d16109f7af48c5b0004ccb8
Wiebke Bienk <wiebke.bienk@gmail.com>
2021-12-07 22:34:53 +0100

    Aufgabe e erledigt

--- grundschulmathe.js
+++ grundschulmathe.js
@@ -1,7 +1,7 @@
 //----Aufgabe A----//
 alert("Grundschulmathe: Dir werden nacheinander Aufgaben gestellt. Beantworte mindestens vier von fünf richtig, um in das nächste Level zu aufzusteigen!");
 
-//----Aufgabe B und C----//
+//----Aufgabe B und C und D und E----//
 
 //Zufallszahl (zwischen 1 und 20)
 function randomNum() {
@@ -11,7 +11,9 @@ function randomNum() {
 //Punkte am Anfang auf 0
 points = 0;
 
-//Funktion Rechenaufgabe
+
+
+//Funktion Addition
 function sum(points = 0, runs = 0) {
     var num1 = randomNum();
     var num2 = randomNum();
@@ -29,12 +31,12 @@ function sum(points = 0, runs = 0) {
         }
         //ins nächste Level
         else if (runs === 5 && points >= 4){
-            alert("Du hast alle Aufgaben beantwortet und" + points + "/5 Punkten erreicht! Damit hast du das nächste Level erreicht!")
+            alert("Du hast alle Aufgaben beantwortet und" + points + "/5 Punkte erreicht! Damit hast du das nächste Level erreicht!")
             subtraktion();
         }
         //nicht ins nächste Level
         else if (runs === 5 && points < 4) {
-            alert("Du hast alle Aufgaben beantwortet und" + points + "/5 Punkten erreicht! Damit hast du leider zu wenige Punkte für das nächste Level! Du kannst jetzt von vorne anfangen. Versuch es einfach nochmal!")
+            alert("Du hast alle Aufgaben beantwortet und" + points + "/5 Punkte erreicht! Damit hast du leider zu wenige Punkte für das nächste Level! Du kannst jetzt von vorne anfangen. Versuch es einfach nochmal!")
             sum();
         }
     }
@@ -48,12 +50,12 @@ function sum(points = 0, runs = 0) {
         }
         //ins nächste Level
         else if (runs === 5 && points >= 4){
-            alert("Du hast alle Aufgaben beantwortet und" + points + "/5 Punkten erreicht! Damit hast du das nächste Level erreicht!")
+            alert("Du hast alle Aufgaben beantwortet und" + points + "/5 Punkte erreicht! Damit hast du das nächste Level erreicht!")
             subtraktion();
         }
         //nicht ins nächste Level
         else if (runs === 5 && points < 4) {
-            alert("Du hast alle Aufgaben beantwortet und" + points + "/5 Punkten erreicht! Damit hast du leider zu wenige Punkte für das nächste Level! Du kannst jetzt von vorne anfangen. Versuch es einfach nochmal!")
+            alert("Du hast alle Aufgaben beantwortet und" + points + "/5 Punkte erreicht! Damit hast du leider zu wenige Punkte für das nächste Level! Du kannst jetzt von vorne anfangen. Versuch es einfach nochmal!")
             sum();
         }
     }
@@ -67,7 +69,7 @@ function subtraktion(points = 0, runs = 0) {
     var min = Math.min(num1, num2);
     var solution = max - min;
     runs++;
-    var userAnswer = parseInt(prompt("Wieviel ist " + max + " - " + min + " ?"));
+    var userAnswer = parseInt(prompt("Wie viel ist " + max + " - " + min + " ?"));
 
     //richtige Antwort
     if (userAnswer === solution) {
@@ -79,8 +81,12 @@ function subtraktion(points = 0, runs = 0) {
         }
         //alle Fragen beantwortet
         else if (runs === 5 && points >= 4){
-            alert("Du hast alle Aufgaben beantwortet und" + points + "/5 Punkten erreicht! Du hast die Aufgabe bestanden")
-            return 0;
+            alert("Du hast alle Aufgaben beantwortet und" + points + "/5 Punkte erreicht! Du hast die Aufgabe bestanden und kommst in das nächste Level")
+            multiplikation();
+        }
+        else if (runs === 5 && points < 4) {
+            alert("Du hast alle Aufgaben in diesem Level beantwortet und" + points + "/5 Punkte erreicht! Damit hast du leider zu wenige Punkte für das nächste Level! Du kannst jetzt von vorne anfangen. Versuch es einfach nochmal!")
+            subtraktion();
         }
     }
 
@@ -93,12 +99,114 @@ function subtraktion(points = 0, runs = 0) {
         }
         //alle Fragen beantwortet
         else if (runs === 5 && points >= 4){
-            alert("Du hast alle Aufgaben beantwortet und" + points + "/5 Punkte erreicht.")
-            return 0;
+            alert("Du hast alle Aufgaben in diesem Level beantwortet und" + points + "/5 Punkte erreicht, damit kommst du in das nächste Level!.")
+            multiplikation();
+        }
+        else if (runs === 5 && points < 4) {
+            alert("Du hast alle Aufgaben in diesem Level beantwortet und" + points + "/5 Punkte erreicht! Damit hast du leider zu wenige Punkte für das nächste Level! Du kannst jetzt von vorne anfangen. Versuch es einfach nochmal!")
+            subtraktion();
         }
     } 
 }
 
+//Multiplikation
+function multiplikation(points = 0, runs = 0) {
+    var num1 = randomNum();
+    var num2 = randomNum();
+    var solution = num1 * num2;
+    runs++;
+    var userAnswer = parseInt(prompt("Wie viel ist " + num1 + " * " + num2 + " ?"), 10);
+
+    //Richtige Antwort
+    if (userAnswer === solution) {
+        points += 1
+        alert("Richtig! Du hast" + points + "/5 Punkte");
+        //bei unter 5 Runden nächste Aufgabe
+        if (runs < 5) {
+            multiplikation(points, runs);
+        }
+        //alle Aufgaben beantwortet
+        else if (runs === 5 && points >= 4) {
+            alert("Du hast alle Aufgaben in diesem Level beantwortet und" + points + "/5 Punkte erreicht! Du hast die Aufgabe bestanden und kommst in das nächste Level")
+            Division();
+        }
+        else if (runs === 5 && points < 4) {
+            alert("Du hast alle Aufgaben in diesem Level beantwortet und" + points + "/5 Punkte erreicht! Damit hast du leider zu wenige Punkte für das nächste Level! Du kannst jetzt von vorne anfangen. Versuch es einfach nochmal!")
+            multiplikation();
+        }
+    }
+    //Falsche Antwort
+    else if (userAnswer !== solution) {
+        alert("Falsch! Du hast" + points + "/5 Punkte");
+        //bei unter 5 RUnden nächste Aufgabe
+        if (runs < 5) {
+            multiplikation(points, numberOfRuns);
+        }
+        //alle Aufgaben beantwortet
+        else if (runs === 5 && points >= 4) {
+            alert("Du hast alle Aufgaben in diesem Level beantwortet und" + points + "/5 Punkte erreicht! Du hast die Aufgabe bestanden und kommst in das nächste Level")
+            Division();
+        }
+        else if (runs === 5 && points < 4) {
+            alert("Du hast alle Aufgaben in diesem Level beantwortet und" + points + "/5 Punkte erreicht! Damit hast du leider zu wenige Punkte für das nächste Level! Du kannst jetzt von vorne anfangen. Versuch es einfach nochmal!")
+            multiplikation();
+        }
+
+    }
+}
+
+//Division
+function Division(points = 0, runs = 0) {
+    var num1 = randomNum();
+    var num2 = randomNum();
+    var solution = num1 / num2;
+    
+    //NUmmer 1 und 2 dürfen keinen Rest hinterlassen (Ganze Zahlen als Ergebnis)
+    if (num1 % num2 == 0) {
+        runs++;
+        var userAnswer = parseInt(prompt("Wieviel ist " + num1 + " / " + num2 + " ?"), 10);
+        //Richtige Antwort
+        if (userAnswer === solution) {
+            points += 1
+            alert("Richtig! Du hast " + points + "/5 Punkte");
+            //Bei unter 5 Runden nächste Aufgabe
+            if (runs < 5) {
+                Division(points, runs);
+            }
+            //Alle Aufgaben erledigt
+            else if (runs === 5 && points >= 4) {
+                alert("Du hast alle Aufgaben in diesem Level beantwortet und" + points + "/5 Punkte erreicht! Du hast Grundschulmathe abgeschlossen")
+                return 0;
+            }
+            else if (runs === 5 && points < 4) {
+                alert("Du hast alle Aufgaben in diesem Level beantwortet und" + points + "/5 Punkte erreicht! Damit hast du leider zu wenige Punkte, um Grundschulmathe abzuschließen! Du kannst jetzt von vorne anfangen. Versuch es einfach nochmal!")
+                Division();
+            }
+        }
+        //Falsche Antwort
+        else if (userAnswer !== solution) {
+            alert("Falsch! Du hast " + points + "/5 Punkte");
+            //bei unter 5 Runden nächste Aufgabe
+            if (runs < 5) {
+                Division(points, runs);
+            }
+            //alle Aufgaben erledigt
+            else if (runs === 5 && points >= 4) {
+                alert("Du hast alle Aufgaben in diesem Level beantwortet und" + points + "/5 Punkte erreicht! Du hast Grundschulmathe abgeschlossen")
+                return 0;
+            }
+            else if (runs === 5 && points < 4) {
+                alert("Du hast alle Aufgaben in diesem Level beantwortet und" + points + "/5 Punkte erreicht! Damit hast du leider zu wenige Punkte, um Grundschulmathe abzuschließen! Du kannst jetzt von vorne anfangen. Versuch es einfach nochmal!")
+                Division();
+            }
+    
+        }
+    }
+    //Wenn Rest überbleibt von vorne mit neuen Zahlen 
+    else {
+        Division(points, runs);
+    }
+}
 
 //Aufruf Funktion Matheaufgabe
 sum();
